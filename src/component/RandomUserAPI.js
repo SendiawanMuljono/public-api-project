ec1b8bfa471e6519f7d382478abf59e50942ef63
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2022-06-02 17:29:42 +0700

    ui

---
+++ src/component/RandomUserAPI.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { Component } from "react";
+import { Card } from "react-bootstrap";
+
+export default class RandomUserAPI extends Component{
+    state={
+        status: []
+    }
+
+    componentDidMount(){
+        axios.get("https://api.randomuser.me/?nat=US&results=5")
+            .then(res => {
+                this.setState({
+                    status: res.status
+                })
+            });
+    }
+
+    render(){
+        const status = this.state.status;
+        return(
+            <Card style={{ width: '18rem' }} bg={status ? "success" : "danger"} text="light">
+                <Card.Body>
+                    <Card.Title>Random User API</Card.Title>
+                    <Card.Subtitle className="mb-2 text-light fst-italic">https://api.randomuser.me/?nat=US&results=5</Card.Subtitle>
+                    <Card.Text>
+                        Status: {this.state.status}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
