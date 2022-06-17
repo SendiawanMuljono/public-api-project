770342d984d44413c87ada2a93fd2dd30131bdc7
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2022-06-17 14:35:22 +0700

    Update JsonPostsAPI.js

--- src/component/JsonPostsAPI.js
+++ src/component/JsonPostsAPI.js
@@ -1,47 +1,38 @@
 import axios from "axios";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 
-export default class JsonPostsAPI extends Component{
-    constructor(props){
-        super(props);
-        
-        this.state={
-            status: [],
-            error: []
-        }
-    }
+export default function JsonPostsAPI(){
+    const [status, setStatus] = useState([]);
+    const [error, setError] = useState([]);
 
-    componentDidMount(){
+    const getApi = () => {
         axios({
             method: 'get',
             url: "https://jsonplaceholder.typicode.com/posts",
         })
         .then(res => {
-            this.setState({
-                status: res.status 
-            })
+            setStatus(res.status);
             console.log(res);
         })
         .catch(err => {
-            this.setState({
-                error: err.message + " (" + err.code + ")"
-            })
+            setError(err.message + " (" + err.code + ")");
         });
     }
 
-    render(){
-        const status = this.state.status;
-        return(
-            <Card style={{ width: '18rem' }} bg={status.length === 0 ? "danger" : "success"} text="light">
-                <Card.Body>
-                    <Card.Title>Jsonplaceholder Posts API</Card.Title>
-                    <Card.Subtitle className="mb-2 text-light fst-italic">https://jsonplaceholder.typicode.com/posts</Card.Subtitle>
-                    <Card.Text>
-                        Status: {status.length === 0 ? this.state.error : this.state.status}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        );
-    }
+    useEffect(() => {
+        getApi();
+    })
+
+    return(
+        <Card style={{ width: '18rem' }} bg={status.length === 0 ? "danger" : "success"} text="light">
+            <Card.Body>
+                <Card.Title>Jsonplaceholder Posts API</Card.Title>
+                <Card.Subtitle className="mb-2 text-light fst-italic">https://jsonplaceholder.typicode.com/posts</Card.Subtitle>
+                <Card.Text>
+                    Status: {status.length === 0 ? error : status}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
 }
